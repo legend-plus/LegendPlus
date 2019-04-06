2bd97bca9f08c9b89af19c0af51c1a968bd116b5
HolyAntimony <maya@alyssa.ws>
2019-04-06 11:19:08 -0400

    Discord Integration and Stuff
    lol

---
+++ Legend Plus/Assets/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Frame");
+    }
+}
